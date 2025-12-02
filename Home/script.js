9108a067830a9075f24741008f8b52bcc40530b6
Krlvin <kelvin.net6@gmail.com>
2025-12-02 16:40:06 +0200

    Update script.js

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,3 @@
-  const supabaseUrl = "https://nhyucbgjocmwrkqbjjme.supabase.co";
-  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oeXVjYmdqb2Ntd3JrcWJqam1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0OTQzNjAsImV4cCI6MjA3OTA3MDM2MH0.uu5ZzSf1CHnt_l4TKNIxWoVN_2YCCoxEZiilB1Xz0eE";
-  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
   // ———— AUTH STATE LISTENER (IMPORTANT) ————
   let currentUser = null;
   let isAdmin = false;
