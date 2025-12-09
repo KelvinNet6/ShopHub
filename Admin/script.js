839bd6e62937cd92ba0a0be956c792155a5999f2
Krlvin <kelvin.net6@gmail.com>
2025-12-09 08:49:21 +0200

    Update script.js

--- Admin/script.js
+++ Admin/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   const supabaseUrl = "https://nhyucbgjocmwrkqbjjme.supabase.co";
-  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oeXVjYmdqb2Ntd3JrcWJqam1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0OTQzNjAsImV4cCI6MjA3OTA3MDM2MH0.uu5ZzSf1CHnt_l4TKNIxWoVN_2YCCoxEZiilB1Xz0eE";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oeXVjYmdqb2Ntd3JrcWJqam1lIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzQ5NDM2MCwiZXhwIjoyMDc5MDcwMzYwfQ.P7qGiTQEpTUyImofEUIYVjEYTbtUOaIn3iYPSX9Mw9Y";
 
   // Correct way to create client
   const { createClient } = supabase;
